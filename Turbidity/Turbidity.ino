77b2b48f396d753cde6b244430b891ea750fe4e4
StephenCatsamas <seca8193@gmail.com>
2020-07-21 16:45:44 +1000

    added case design

--- Turbidity/Turbidity.ino
+++ Turbidity/Turbidity.ino
@@ -29,6 +29,17 @@
 
 
 #include "memorysaver.h"
+
+
+#define BMPIMAGEOFFSET 66
+const char bmp_header[BMPIMAGEOFFSET] PROGMEM =
+{
+  0x42, 0x4D, 0x36, 0x58, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x42, 0x00, 0x00, 0x00, 0x28, 0x00,
+  0x00, 0x00, 0x40, 0x01, 0x00, 0x00, 0xF0, 0x00, 0x00, 0x00, 0x01, 0x00, 0x10, 0x00, 0x03, 0x00,
+  0x00, 0x00, 0x00, 0x58, 0x02, 0x00, 0xC4, 0x0E, 0x00, 0x00, 0xC4, 0x0E, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF8, 0x00, 0x00, 0xE0, 0x07, 0x00, 0x00, 0x1F, 0x00,
+  0x00, 0x00
+};
 //This demo can only work on OV5640_MINI_5MP_PLUS or OV5642_MINI_5MP_PLUS platform.
 
 // #ifndef SDCARD_SS_PIN
@@ -42,6 +53,7 @@ const int CS = 7;
 int capture_count = 0;
 bool is_header = false;
 int total_time = 0;
+String fname;
 
 ArduCAM myCAM( OV2640, CS );
 
@@ -49,25 +61,30 @@ RTCZero rtc;
 
 uint8_t read_fifo_burst(ArduCAM myCAM);
 
+void wakeUp(){}
 
 void setup() {
+   pinMode(4, INPUT);
+	attachInterrupt(digitalPinToInterrupt(4), wakeUp, LOW); 
   // put your setup code here, to run once:
   
 
 
   Wire.begin();
   
-  rtc.begin();
+	rtc.begin();
   
- rtc.setTime(0, 0, 0);
- rtc.setDate(0, 0, 0);  
+	rtc.setTime(0, 0, 0);
+	rtc.setDate(0, 0, 0);  
   
 
-  Serial1.begin(115200);
-  Serial1.println(F("ArduCAM Start!"));
+  Serial1.begin(9600);
+  Serial1.println(F("R"));
   // set the CS as an output:
   pinMode(CS, OUTPUT);
   digitalWrite(CS, HIGH);
+
+  
   // initialize SPI:
   SPI.begin();
   
@@ -77,22 +94,23 @@ void setup() {
   //Initialize SD Card
   while (!SD.begin(SDCARD_SS_PIN))
   {
-    Serial1.println(F("SD Card Error!")); delay(1000);
+    Serial1.println(F("SD Card Error!"));
+	delay(1000);
   }
-  Serial1.println(F("SD Card detected."));
-  //Change to JPEG capture mode and initialize the OV5640 module
-  myCAM.set_format(JPEG);
-  myCAM.InitCAM();
-  myCAM.clear_fifo_flag();
-  myCAM.write_reg(ARDUCHIP_FRAMES, FRAMES_NUM);
+  //Serial1.println(F("SD Card detected."));
+
 }
 
 void camWake(){
 	uint8_t vid, pid;
 	uint8_t temp;
 	
+	Serial1.println("W");
+	pinMode(5, OUTPUT);
+	digitalWrite(5, LOW);
+	delay(20);
 	
-	 myCAM.clear_bit(ARDUCHIP_GPIO,GPIO_PWDN_MASK);
+	 //myCAM.clear_bit(ARDUCHIP_GPIO,GPIO_PWDN_MASK);
   
 	
 	//Reset the CPLD
@@ -109,7 +127,8 @@ delay(100);
       Serial1.println(F("SPI interface Error!"));
       delay(1000); continue;
     } else {
-      Serial1.println(F("SPI interface OK.")); break;
+      //Serial1.println(F("SPI interface OK."));
+	  break;
     }
   }
 
@@ -123,19 +142,46 @@ delay(100);
       delay(1000); continue;
     }
     else {
-      Serial1.println(F("ACK CMD OV2640 detected.")); break;
+      //Serial1.println(F("ACK CMD OV2640 detected."));
+	  break;
     }
   }
-	
+	  //Change to JPEG capture mode and initialize the OV5640 module
+  myCAM.set_format(BMP);
+  myCAM.InitCAM();
+  myCAM.OV2640_set_Light_Mode(Sunny);
+  // myCAM.OV2640_set_Color_Saturation(Saturation0);
+  // myCAM.OV2640_set_Brightness(Brightness0);
+  // myCAM.OV2640_set_Contrast(Contrast0);
+  // myCAM.OV2640_set_Special_effects(Normal);
+ //myCAM.write_reg(ARDUCHIP_FRAMES, FRAMES_NUM);
+ ////take dummy shot to get exposure right
+  // myCAM.flush_fifo();
+  // myCAM.clear_fifo_flag();
+  // myCAM.start_capture();
+  // while ( !myCAM.get_bit(ARDUCHIP_TRIG, CAP_DONE_MASK));
+  // myCAM.flush_fifo();
+  // myCAM.clear_fifo_flag();
+  
+  
+  
+  
+  
 }
 
 void camSleep(){
-	 myCAM.set_bit(ARDUCHIP_GPIO,GPIO_PWDN_MASK);
+	 digitalWrite(5, HIGH);
 }
 
-void loop() {
+void capture(){
+	
+	
+	
+	uint8_t reg;
+	camWake();
+ for (int iter = 0; iter < 5; iter++){
   
-  uint8_t reg;
+  
 	
   total_time = millis();
   
@@ -143,10 +189,10 @@ void loop() {
   myCAM.flush_fifo();
   myCAM.clear_fifo_flag();
 
-  myCAM.OV2640_set_JPEG_size(OV2640_640x480);
+  
 
   //Start capture
-  Serial1.println(F("start capture"));
+   //Serial1.println(F("start capture"));
   
 
   myCAM.start_capture();
@@ -155,134 +201,138 @@ void loop() {
   
   read_fifo_burst(myCAM);
   
-  Serial1.println(F("saved capture"));
+  //Serial1.println(F("saved capture"));
  
 
   //Clear the capture done flag
   myCAM.clear_fifo_flag();
   
   total_time -= millis();
-  total_time += 1000;
+  total_time += 2000;
   if (total_time > 0){
   delay(total_time);
   }else{
   }
   
-  Serial1.print(F("CAM Capture Done: "));
-   Serial1.println(total_time, DEC);
-
-  if(capture_count > 5){
-  Serial1.print(F("Sleep"));  
+  // Serial1.print(F("CAM Capture Done: "));
+   // Serial1.println(total_time, DEC);
+}
+  
+  Serial1.print(F("S"));  
   camSleep();
+	
+}
+
+void loop() {
+  
+  
  
 
-  rtc.attachInterrupt(alarmMatch);
+    rtc.attachInterrupt(alarmMatch);
    
-   uint32_t now = rtc.getEpoch();
-	rtc.setAlarmEpoch(now + 15);
+    uint32_t now = rtc.getEpoch();
+	rtc.setAlarmEpoch(now + 3600);
 	rtc.enableAlarm(rtc.MATCH_HHMMSS);
 
 
    rtc.standbyMode();
+   
+   sendimg();
+   
+  capture();
   
-  camWake();
   
-  
-  capture_count = 0;
-  }
-  capture_count++;
 }
 
+void sendimg(){
+	Serial1.print(fname);
+	Serial1.print('T');
+}
 
 
 
 uint8_t read_fifo_burst(ArduCAM myCAM)
 {
-  uint8_t temp = 0, temp_last = 0;
   uint32_t length = 0;
   static int i = 0;
   static int k = 0;
+  
+  char VH, VL;
+  int row  = 0;
+  int col  = 0;
   char str[16];
   File outFile;
   File logFile;
   byte buf[256];
   length = myCAM.read_fifo_length();
-  Serial1.print(F("The fifo length is :"));
-  Serial1.println(length, DEC);
+  //Serial1.print(F("The fifo length is :"));
+  //Serial1.println(length, DEC);
   if (length >= MAX_FIFO_SIZE) //8M
   {
-    Serial1.println("Over size.");
+    //Serial1.println("Over size.");
     return 0;
   }
   if (length == 0 ) //0 kb
   {
-    Serial1.println(F("Size is 0."));
+    //Serial1.println(F("Size is 0."));
     return 0;
   }
-  myCAM.CS_LOW();
-  myCAM.set_fifo_burst();//Set fifo burst mode
-  i = 0;
-  while ( length-- )
-  {
-    temp_last = temp;
-    temp =  SPI.transfer(0x00);
-    //Read JPEG data from FIFO
-    if ( (temp == 0xD9) && (temp_last == 0xFF) ) //If find the end ,break while,
-    {
-      buf[i++] = temp;  //save the last  0XD9
-      //Write the remain bytes in the buffer
-      myCAM.CS_HIGH();
-      outFile.write(buf, i);
-      //Close the file
-      outFile.close();
-      Serial1.println(F("OK"));
-      is_header = false;
-      myCAM.CS_LOW();
-      myCAM.set_fifo_burst();
-      i = 0;
-    }
-    if (is_header == true)
-    {
-      //Write image data to buffer if not full
-      if (i < 256)
-        buf[i++] = temp;
-      else
-      {
-        //Write 256 bytes image data to file
-        myCAM.CS_HIGH();
-        outFile.write(buf, 256);
-        i = 0;
-        buf[i++] = temp;
-        myCAM.CS_LOW();
-        myCAM.set_fifo_burst();
-      }
-    }
-    else if ((temp == 0xD8) & (temp_last == 0xFF))
-    {
-      is_header = true;
-      myCAM.CS_HIGH();
+  
+	 myCAM.CS_HIGH();
       //Create a avi file
       k = k + 1;
       itoa(k, str, 10);
-      strcat(str, ".jpg");
+      strcat(str, ".bmp");
+	 
+	  fname = str;
+	 
       //Open the new file
       outFile = SD.open(str, O_WRITE | O_CREAT | O_TRUNC);
       if (! outFile)
       {
-        Serial1.println(F("File open failed"));
+        //Serial1.println(F("File open failed"));
         while (1);
       }
-      myCAM.CS_LOW();
-      myCAM.set_fifo_burst();
-      buf[i++] = temp_last;
-      buf[i++] = temp;
-    }
+  
+    myCAM.CS_LOW();
+    myCAM.set_fifo_burst();//Set fifo burst mode
+  
+  for ( i = 0; i < BMPIMAGEOFFSET; i++){
+	  buf[i] = pgm_read_byte(&bmp_header[i]);
   }
+  
+    
+  for (row = 0; row < 240; row++)
+      {
+        for (col = 0; col < 320; col++)
+        {
+          VH = SPI.transfer(0x00);
+          VL = SPI.transfer(0x00);
+          
+		  buf[i++] = VL;
+		  buf[i++] = VH;
+			
+		  if (i > 254)
+		  {
+			myCAM.CS_HIGH();
+			outFile.write(buf, i);
+			i = 0;
+			myCAM.CS_LOW();
+			myCAM.set_fifo_burst();
+		  }
+        }
+      }
+	myCAM.CS_HIGH();
+	outFile.write(buf, i);
+	//Close the file
+	outFile.close();
+	//Serial1.println(F("OK"));
+
   myCAM.CS_HIGH();
   logFile = SD.open("time.csv", FILE_WRITE);
       if (! logFile)
       {
-        Serial1.println(F("Log File open failed"));
+        //Serial1.println(F("Log File open failed"));
         while (1);
       }
   logFile.print(str);
@@ -300,7 +350,20 @@ uint8_t read_fifo_burst(ArduCAM myCAM)
  logFile.print(rtc.getYear());
   logFile.println();
   logFile.close();
-  Serial1.println(F("time logged"));
+  //Serial1.println(F("time logged"));
+
+	fname += ',';
+	fname += rtc.getSeconds();
+	fname += ',';
+	fname += rtc.getMinutes();
+	fname += ',';
+	fname += rtc.getHours();
+	fname += ',';
+	fname += rtc.getDay();
+	fname += ',';
+	fname += rtc.getMonth();
+	fname += ',';
+	fname += rtc.getYear();
 
   myCAM.CS_LOW();
   
